9eb65ef93d8206adeda9faf5abb9194fc6e24b83
mknnishiko <75335392+mknnishiko@users.noreply.github.com>
2022-07-08 14:08:15 +0900

    [fix] CreateBoardController.php

--- controller/CreateBoardController.php
+++ controller/CreateBoardController.php
@@ -6,7 +6,7 @@ require_once  __DIR__ . '/../libs/dao/BoardDao.php';
 class CreateBoardController extends BaseController {
 
     // 読み込むテンプレートファイルを設定
-    protected $template = 'templates/board.tpl';
+    protected $template = 'templates/create_board.tpl';
 
     // ログイン必須
     protected $isLogin = True;
